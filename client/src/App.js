25aa09778ae8914b094897a984ae3313df8a4ea9
MohammedYounis2001 <mohammedyounis20011@gmail.com>
2023-12-20 15:28:07 +0300

    new vesion

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
+// import { Home } from "./pages/home";
 import { NavBar } from "./components/uiPrmitives/NavBar";
 import { Footer } from "./components/uiPrmitives/Footer";
+import Job from "./pages/Job";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <NavBar />
+        {/* <NavBar /> */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/job" element={<Job />} />
+          
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </BrowserRouter>
     </>
   );
